aef99f10be25244ea3ec5abbcb78dfaadba86f4c
clibequilibrium <105135724+clibequilibrium@users.noreply.github.com>
2023-10-21 13:17:49 -0400

    Changed gitignore

--- src/cs/samples/HelloWorld/Program.cs
+++ src/cs/samples/HelloWorld/Program.cs
@@ -1,16 +1,15 @@
+using System.Threading;
 using bottlenoselabs.C2CS.Runtime;
-using Tracy;
 using static Tracy.PInvoke;
+
 internal sealed class Program
 {
-    internal unsafe static void Main(string[] args)
+    internal static unsafe void Main(string[] args)
     {
         while (true)
         {
             TracyEmitFrameMark((CString)("FrameUpdate"));
             Thread.Sleep(16);
         }
-		
-        return;
     }
 }
