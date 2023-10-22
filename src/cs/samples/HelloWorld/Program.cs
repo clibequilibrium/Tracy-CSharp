816c02aa8d104ef69596485ce1c7818275870ac0
clibequilibrium <105135724+clibequilibrium@users.noreply.github.com>
2023-10-22 15:41:25 -0400

    Added better sample

--- src/cs/samples/HelloWorld/Program.cs
+++ src/cs/samples/HelloWorld/Program.cs
@@ -1,15 +1,46 @@
+using System;
 using System.Threading;
 using bottlenoselabs.C2CS.Runtime;
 using static Tracy.PInvoke;
 
 internal sealed class Program
 {
+    static void Delay()
+    {
+        int i, end;
+        double j = 0;
+
+        using (Profiler.BeginEvent("My Custom Event Name"))
+        {
+
+            Random random = new Random();
+
+            for (i = 0, end = (int)random.NextInt64() / 100; i < end; ++i)
+            {
+                j += Math.Sin(i);
+            }
+
+        }
+    }
+
+    static void ColoredEvent()
+    {
+        using (Profiler.BeginEvent(colorType: Profiler.ColorType.Aqua))
+        {
+
+            Thread.Sleep(16);
+        }
+    }
+
     internal static unsafe void Main(string[] args)
     {
         while (true)
         {
-            TracyEmitFrameMark((CString)("FrameUpdate"));
-            Thread.Sleep(16);
+            Delay();
+            ColoredEvent();
         }
+
+        // Call it at the end of your program to free allocated memory
+        Profiler.Dispose();
     }
 }
