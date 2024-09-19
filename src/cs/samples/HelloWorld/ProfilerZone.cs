9e8c740c157a8b33e87140ccef17bdfb5231aa9d
Mervill <mervills.email@gmail.com>
2024-09-18 20:54:42 -0700

    Update the hello world example.

---
+++ src/cs/samples/HelloWorld/ProfilerZone.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static Tracy.PInvoke;
+
+public readonly struct ProfilerZone : IDisposable
+{
+    public readonly TracyCZoneCtx Context;
+
+    internal ProfilerZone(TracyCZoneCtx context)
+    {
+        Context = context;
+    }
+
+    public void EmitName(string name)
+    {
+        var (namestr, nameln) = Profiler.GetCString(name);
+        TracyEmitZoneName(Context, namestr, nameln);
+    }
+
+    public void EmitColor(uint color)
+    {
+        TracyEmitZoneColor(Context, color);
+    }
+
+    public void EmitText(string text)
+    {
+        var (textstr, textln) = Profiler.GetCString(text);
+        TracyEmitZoneText(Context, textstr, textln);
+    }
+
+    public void Dispose()
+    {
+        TracyEmitZoneEnd(Context);
+    }
+}
